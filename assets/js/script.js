5997aa256de3a31fa8f9196f471c4e646b5d4cad
Brittany Moes <brittanylee@Brittanys-MacBook-Pro.local>
2020-09-25 22:40:38 -0500

    added event to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
+    z
+
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
-
-buttonEl.addEventListener("click", createTaskHandler);{
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
 };
+
+formEl.addEventListener("submit", createTaskHandler);
+
+
 
